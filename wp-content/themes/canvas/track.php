ebe82781ef414ca916400fec970488ee788cfe68
Nick Wynja <nick@nickwynja.com>
2013-11-12 22:53:56 -0500

    Fixes track service

--- wp-content/themes/canvas/track.php
+++ wp-content/themes/canvas/track.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysql_connect('localhost','root','raspberry') or die('Could not connect.');
-mysql_select_db('villagescience',$conn);
+mysql_select_db('vspi_local',$conn);
 $url = $_POST['url'];
 $string = "insert into vs_track(url, time_viewed) values('".$url."', now())";
 mysql_query($string,$conn);
